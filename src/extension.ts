4c2325a64e6d02ae29bfdada93cbb9e90d16afb6
Nils Ballmann <nils.ballmann@gmail.com>
2017-07-16 15:39:37 +0200

    the update on save functionality can now be enabled and disabled via the config

--- src/extension.ts
+++ src/extension.ts
@@ -7,6 +7,7 @@ class PythonCodingToolsExtension {
     private _context: vscode.ExtensionContext;
     private _variableName: string;
     private _dateTimeFmt: string;
+    private _enableOnSave: boolean;
 
     constructor(context: vscode.ExtensionContext) {
         this._context = context;
@@ -31,45 +32,78 @@ class PythonCodingToolsExtension {
         let pattern = '^' + this._variableName + '(\\s*)=(\\s*)([\'"])(.*)([\'"])$'
         let re = new RegExp(pattern);
 
+        let lineNumber = null;
+        let colStart = null;
+        let colEnd = null;
         for (let i = 0; i < document.lineCount; i++) {
             let line = document.lineAt(i);
+            if (line.isEmptyOrWhitespace) {
+                continue;
+            }
             let match = re.exec(line.text);
             if (match != null) {
-                var lineNumber = line.lineNumber;
-                var colStart = this._variableName.length + match[1].length + 1 + match[2].length + match[3].length;
-                var colEnd = colStart + match[4].length;
+                lineNumber = line.lineNumber;
+                colStart = this._variableName.length + match[1].length + 1 + match[2].length + match[3].length;
+                colEnd = colStart + match[4].length;
                 break;
             }
         }
 
-        let rangeToReplace = new vscode.Range(new vscode.Position(lineNumber, colStart), new vscode.Position(lineNumber, colEnd));
-        let textToReplace = moment().format(this._dateTimeFmt);
+        if (!(lineNumber == null || colStart == null || colEnd == null)) {
+            let rangeToReplace = new vscode.Range(new vscode.Position(lineNumber, colStart), new vscode.Position(lineNumber, colEnd));
+            let textToReplace = moment().format(this._dateTimeFmt);
 
-        editor.edit(function (edit) {
-            edit.replace(rangeToReplace, textToReplace);
-        });
+            editor.edit(function (edit) {
+                edit.replace(rangeToReplace, textToReplace);
+            });
+        }
     }
 
     public loadConfig(): void {
-        this._dateTimeFmt = <any>vscode.workspace.getConfiguration('python-coding-tools.dateTimeFmt');
-        this._variableName = <any>vscode.workspace.getConfiguration('python-coding-tools.variableName');
+        let config = vscode.workspace.getConfiguration('python-coding-tools.update-module-variable');
+        this._enableOnSave = config.get<boolean>('enableOnSave');
+        this._dateTimeFmt = config.get<string>('dateTimeFmt');
+        this._variableName = config.get<string>('variableName');
+    }
+
+    public isEnabledOnSave(): boolean {
+        return this._enableOnSave;
     }
 }
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('python-coding-tools activated');
-
     var extension = new PythonCodingToolsExtension(context);
 
     context.subscriptions.push(vscode.commands.registerCommand('extension.python-coding-tools.update-module-variable', () => {
         extension.updateModuleVariable(vscode.window.activeTextEditor.document);
     }));
 
-    context.subscriptions.push(vscode.workspace.onWillSaveTextDocument((event: vscode.TextDocumentWillSaveEvent) => {
-        vscode.commands.executeCommand('extension.python-coding-tools.update-module-variable');
+    let onWillSave = null;
+    if (extension.isEnabledOnSave()) {
+        onWillSave = vscode.workspace.onWillSaveTextDocument((event: vscode.TextDocumentWillSaveEvent) => {
+            vscode.commands.executeCommand('extension.python-coding-tools.update-module-variable');
+        });
+        context.subscriptions.push(onWillSave);
+    }
+
+    context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(() => {
+        extension.loadConfig();
+
+        if (extension.isEnabledOnSave()) {
+            if (onWillSave == null) {
+                context.subscriptions.push(vscode.workspace.onWillSaveTextDocument((event: vscode.TextDocumentWillSaveEvent) => {
+                    vscode.commands.executeCommand('extension.python-coding-tools.update-module-variable');
+                }));
+            }
+        } else {
+            if (onWillSave != null) {
+                onWillSave.dispose();
+                onWillSave = null;
+            }
+        }
     }));
+
 }
 
 export function deactivate() {
-    console.log('python-coding-tools deactivated')
 }
